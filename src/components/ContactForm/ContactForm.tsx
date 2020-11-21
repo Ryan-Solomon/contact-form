b066a8e6e719c4091ead36dc5fc735c573aa7bd2
Ryan-Solomon <ryanjsolomon24@gmail.com>
2020-11-21 17:37:48 -0600

    lay out form

--- src/components/ContactForm/ContactForm.tsx
+++ src/components/ContactForm/ContactForm.tsx
@@ -5,54 +5,59 @@ const ContactForm = () => {
   return (
     <main className='form-container'>
       <h1>CONTACT</h1>
-      <section className='form-info'>
-        <h2>INFORMATION</h2>
-        <h4>Lorem, ipsum dolor sit amet elit. Facilis, nam.</h4>
-        <h4>Lorem, ipsum dolor sit amet elit. Facilis, nam.</h4>
-        <h4>Lorem, ipsum dolor sit amet elit. Facilis, nam.</h4>
-      </section>
-      <section className='faq-thingy'>
-        <div className='card'>
-          <h4>Lorem ipsum dolor sit amet.</h4>
-        </div>
-        <div className='card'>
-          <h4>Lorem ipsum dolor sit amet.</h4>
-        </div>
-        <div className='card'>
-          <h4>Lorem ipsum dolor sit amet.</h4>
-        </div>
-        <div className='card'>
-          <h4>Lorem ipsum dolor sit amet.</h4>
-        </div>
-      </section>
+      <div className='top-form-section'>
+        <section className='form-info'>
+          <h2>INFORMATION</h2>
 
-      <h2>CONTACT FORM</h2>
-      <form>
-        <section className='form-section'>
-          <label htmlFor='first-name'>First Name</label>
-          <input id='first-name' placeholder='First Name' type='text' />
+          <h4>Lorem, ipsum dolor sit amet elit. Facilis, nam.</h4>
+          <h4>Lorem, ipsum dolor sit amet elit. Facilis, nam.</h4>
+          <h4>Lorem, ipsum dolor sit amet elit. Facilis, nam.</h4>
         </section>
-        <section className='form-section'>
-          <label htmlFor='last-name'>Last Name</label>
-          <input id='last-name' placeholder='Last Name' type='text' />
+        <section className='faq-thingy'>
+          <div className='card'>
+            <h4>Lorem ipsum dolor sit amet.</h4>
+          </div>
+          <div className='card'>
+            <h4>Lorem ipsum dolor sit amet.</h4>
+          </div>
+          <div className='card'>
+            <h4>Lorem ipsum dolor sit amet.</h4>
+          </div>
+          <div className='card'>
+            <h4>Lorem ipsum dolor sit amet.</h4>
+          </div>
         </section>
-        <section className='form-section'>
-          <label htmlFor='country'>Country</label>
-          <input id='country' placeholder='Country' type='text' />
-        </section>
-        <section className='form-section'>
-          <label htmlFor='email-address'>Email Address</label>
-          <input id='email-address' placeholder='Email Address' type='email' />
-        </section>
-        <section className='form-section'>
-          <label htmlFor='subject'>Subject</label>
-          <input id='subject' placeholder='Subject' type='text' />
-        </section>
-        <section className='form-section'>
-          <label htmlFor='message'>Message</label>
-          <textarea id='message' placeholder='Message' />
-        </section>
-      </form>
+      </div>
+
+      <div className='contact-form-section'>
+        <h2>CONTACT FORM</h2>
+        <form className='form'>
+          <section className='form-section first-name'>
+            <label htmlFor='first-name'>First Name</label>
+            <input id='first-name' type='text' />
+          </section>
+          <section className='form-section last-name'>
+            <label htmlFor='last-name'>Last Name</label>
+            <input id='last-name' type='text' />
+          </section>
+          <section className='form-section'>
+            <label htmlFor='country'>Country</label>
+            <input id='country' type='text' />
+          </section>
+          <section className='form-section'>
+            <label htmlFor='email-address'>Email Address</label>
+            <input id='email-address' type='email' />
+          </section>
+          <section className='form-section subject'>
+            <label htmlFor='subject'>Subject</label>
+            <input id='subject' type='text' />
+          </section>
+          <section className='form-section message'>
+            <label htmlFor='message'>Message</label>
+            <textarea id='message' />
+          </section>
+        </form>
+      </div>
     </main>
   );
 };
